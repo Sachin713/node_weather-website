1422f55cef23dd83a466906199c3de1d9d7b712f
Sachin <s.a.patel713@gmail.com>
2020-03-09 10:07:15 -0400

    added high and low temp of today

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,3 @@
-
 const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/243910959bea61412b00a20f4ef70b9c/' + latitude + ',' + longitude + '?units=si'
@@ -11,7 +10,8 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
             callback(undefined, body.daily.data[0].summary + ' It is currently ' +
-                body.currently.temperature + ' degress out. There is a ' +
+                body.currently.temperature + ' degress out. The high Today is ' + body.daily.data[0].temperatureHigh +
+                ' with  a low of ' + body.daily.data[0].temperatureLow + ' There is a ' +
                 body.currently.precipProbability + '% chance of rain.')
         }
     })
